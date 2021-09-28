a4f7ef51f127dfc00006562ef7da76abfd5c3256
Halil.KAYA <kayaantalya7@gmail.com>
2021-09-28 23:10:13 +0300

    Sample works on HTTP, Delegate pattern, Core Location, Phone Gps

---
+++ GetWeather/Controller/WeatherViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  GetWeather
+
+import UIKit
+import CoreLocation
+
+// MARK:- UIViewController
+class WeatherViewController: UIViewController {
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextField.delegate = self
+        weatherManager.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    @IBAction func pressedCurrentLocation(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+}
+
+// MARK:- Text Field Delegate
+extension WeatherViewController: UITextFieldDelegate {
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let cityName = searchTextField.text {
+            weatherManager.fetchWeather(cityName: cityName)
+        }
+        searchTextField.text = ""
+    }
+}
+
+// MARK:- Weather Manager Delegate
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async { [self] in
+            temperatureLabel.text = weather.temperatureString
+            cityLabel.text = weather.cityName
+            conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func didFailWithErorr(error: Error) {
+        print(error.localizedDescription)
+    }
+}
+
+// MARK:- Location Manager Delegate
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("Got Location")
+            locationManager.stopUpdatingLocation()
+            weatherManager.fetchWeather(lattitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(" Error:- \(error.localizedDescription)")
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+Sample iPhone app to get the weather of city and as well the current location.
+
+## Features
+
+* Dark-mode enabled app with vector assets.
+* Delegate pattern.
+* URLSession to network and make HTTP requests.
+* Core Location to get the current location from the phone GPS.
+
+<p>
+<img src="https://github.com/HALILKAYAA/LocateWeatherApp/blob/main/GetWeather/Snapshot/ANTALYA.png" width="350">
+<img src="https://github.com/HALILKAYAA/LocateWeatherApp/blob/main/GetWeather/Snapshot/LONDON.png" width="350">
+</p>
